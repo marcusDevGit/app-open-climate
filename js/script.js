983f2b3fd4fa3973755b4914f2e8464a1520453c
Marcus Phellypp <marcus_psc@hotmail.com>
2022-12-10 20:58:24 -0300

    Adicionado api de img para fundo no js e  add sugestoes e mod no css/html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Variaveis
 const apiKey = "6577b91dd0810fb8fe3d9ce378124e39";
 const apiCoutryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 //Seleção de elementos
 const cityInput = document.querySelector("#city_input");
 const searchBtn = document.querySelector("#search");
@@ -14,19 +15,56 @@ const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
 const weatherContainer = document.querySelector("#weather_data");
+
+const erorMsgContainer = document.querySelector("#error_msg");
+const loader = document.querySelector("#loader");
+
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionBtn = document.querySelectorAll("#suggestions button");
+
+
+// Loader
+const toggleLoader = () => {
+    loader.classList.toggle("hide");
+};
+
 //Funções
+
 const getWeatherData = async(city) => {
+    toggleLoader();
+
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
     const res = await fetch(apiWeatherURL);
     const data =  await res.json();
+
+    toggleLoader();
     
     return data;
 }
 
-    const showWeatherData = async (city) => {
+// Tratamento dos Erros
+
+const showError = () => {
+    erorMsgContainer.classList.remove("hide");
+};
+
+const hideInfo = () => {
+    erorMsgContainer.classList.add("hide");
+    weatherContainer.classList.add("hide");
+
+    suggestionContainer.classList.add("hide");
+};
+
+const showWeatherData = async (city) => {
+    hideInfo();
 
        const data = await getWeatherData(city);
+
+       if(data.cod === "404") {
+        showError();
+        return;
+       }
 
        cityElement.innerHTML = data.name;
        tempElement.innerHTML = parseInt(data.main.temp);
@@ -36,9 +74,12 @@ const getWeatherData = async(city) => {
        humidityElement.innerText = `${data.main.humidity}%`;
        windElement.innerText = `${data.wind.speed}km/h`;
 
+       //imagens da cidade celecionada
+       document.body.style.backgroundImage = ` url("${apiUnsplash + city}")`;
+
        weatherContainer.classList.remove("hide");
 
-    }
+}
 
 //Eventos
 searchBtn.addEventListener("click", (e) => {
@@ -55,4 +96,15 @@ cityInput.addEventListener("keyup", (e) => {
 
         showWeatherData(city);
     }
-})
+});
+
+//sugestoes
+suggestionBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const city = btn.getAttribute("id");
+
+        showWeatherData(city);
+    });
+    
+});
+
